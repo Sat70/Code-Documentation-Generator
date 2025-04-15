69a66125b5bd558f8f53b3e9a04c021271cfca90
Satam Gupta <162042329+Sat70@users.noreply.github.com>
2025-04-15 22:29:06 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -56,7 +56,7 @@ def generate_doc():
 
 # main function
 def start_web_server():
-    app.run(host="0.0.0.0", port="8080", debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 # Run the application
 if __name__ == '__main__':
